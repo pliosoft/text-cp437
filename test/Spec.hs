be7193a67031bd89172024fafabe496f22257adc
Ignat Insarov <kindaro@gmail.com>
2018-08-15 03:55:27 +0700

    Add test for Graphical.

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,6 +2,7 @@ import Test.QuickCheck
 import qualified Data.Text as T
 import Data.Text.Arbitrary
 import Data.Text.CP437
+import qualified Data.Text.CP437.Graphical as G
 
 compareChars :: (Char, Char) -> Bool
 compareChars (a, b) = a == b || b == '\x0'
@@ -9,6 +10,9 @@ compareChars (a, b) = a == b || b == '\x0'
 propReversableInRange :: T.Text -> Bool
 propReversableInRange t = all compareChars (T.zip t (cp437ToText (textToCp437 t)))
 
+propReversableInRangeGraphical :: T.Text -> Bool
+propReversableInRangeGraphical t = all compareChars (T.zip t (G.cp437ToText (G.textToCp437 t)))
+
 main :: IO ()
-main = quickCheck propReversableInRange
+main = quickCheck propReversableInRange >> quickCheck propReversableInRangeGraphical
 
